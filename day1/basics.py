ab8bbd8f4bf9e21f16011b75ba510234b58e2759
Anusha V <116417089+anushav-12@users.noreply.github.com>
2026-01-17 17:04:44 +0530

    Day 1: Python basics

---
+++ day1/basics.py
@@ -0,0 +1,16 @@
+
+print("Hello");
+print("Day 1 of Python");
+
+name = input("Enter your name:")
+print("Hello,", name);
+
+age = input("What is your age?")
+print("My age is", age);
+
+
+print("\nProfile");
+print("-------");
+print("Name:", name);
+print("Age:", age);
+print("Learning Python step by step");
