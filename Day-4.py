072001b9e43c65acad0de103e79b3272b35746fe
Anusha V <116417089+anushav-12@users.noreply.github.com>
2026-01-30 12:00:03 +0530

    Update Day-4.py

--- Day-4.py
+++ Day-4.py
@@ -3,7 +3,7 @@ for variable in sequence:
 # code to repeat
 
 """
-range() – Backbone of for Loop
+1. range() – Backbone of for Loop
 🔹 range(start, stop, step)
 
 start → where to begin (default = 0)
@@ -44,7 +44,101 @@ Python takes the string "Anusha"
 Reads it one character at a time
 Stores each character in the variable ch
 Each character prints on a new line because print() adds a newline by default
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+
+2. for Loop with Conditions
+
+Print even numbers from 1 to 20
+for num in range(1, 21):
+    if num % 2 == 0:
+        print(num)
+
+Logic:
+% gives remainder
+Even → remainder 0
 -------------------------------------
+
+3. break and continue
+-> break → stop loop completely
+
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+Output:
+1 2 3 4
+-------------------------------------
+
+4. continue → skip current iteration
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+Output:
+1 2 4 5
+-------------------------------------
+
+5. Nested for Loops (Important for Interviews)
+Example: Pattern logic
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
+
+Execution:
+i=0 → j=0,1
+i=1 → j=0,1
+i=2 → j=0,1
+-------------------------------------
+
+6. while Loop (Condition-Based Loop)
+-> What is while loop?
+Runs as long as condition is TRUE
+
+Syntax:
+while condition:
+    # code
+
+Example 1: Print numbers 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+Logic:
+Check condition
+Execute block
+Update variable
+Repeat
+
+⚠️ Forgetting i += 1 → infinite loop
+-------------------------------------
+
+7. Infinite Loop Example 
+while True:
+    print("Hello")
+
+
+Used in:
+Servers
+Games
+Continuous monitoring systems
+-------------------------------------
+
+8. while with break
+i = 1
+while True:
+    if i == 6:
+        break
+    print(i)
+    i += 1
+-------------------------------------
+    
 """
 
 # FOR LOOP PRACTICE – QUESTIONS & ANSWERS
