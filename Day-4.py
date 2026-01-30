e391f79c08ed64cc0fefb801f97167b580a75195
Anusha V <116417089+anushav-12@users.noreply.github.com>
2026-01-30 11:52:38 +0530

    Update Day-4.py

--- Day-4.py
+++ Day-4.py
@@ -7,9 +7,7 @@ range() – Backbone of for Loop
 🔹 range(start, stop, step)
 
 start → where to begin (default = 0)
-
 stop → where to stop (excluded)
-
 step → jump (default = 1)
 
 Examples:
@@ -17,6 +15,7 @@ range(5)          → 0,1,2,3,4
 range(1, 6)       → 1,2,3,4,5
 range(1, 10, 2)   → 1,3,5,7,9
 
+-------------------------------------
 
 Example 1: Print numbers 1 to 5
 for i in range(1, 6):
@@ -24,22 +23,28 @@ for i in range(1, 6):
 
 Logic:
 i takes value 1, prints, then 2, 3, 4, 5, stops before 6.
-
+-------------------------------------
 Example 2: Loop through a list
 languages = ["Python", "SQL", "Java"]
 
 for lang in languages:
     print(lang)
-
-
+    
 Logic:
 lang = "Python"
 then "SQL"
 then "Java"
-
+-------------------------------------
 Example 3: Loop through a string
 for ch in "Anusha":
     print(ch)
+Logic:
+for ch in "Anusha":
+Python takes the string "Anusha"
+Reads it one character at a time
+Stores each character in the variable ch
+Each character prints on a new line because print() adds a newline by default
+-------------------------------------
 """
 
 # FOR LOOP PRACTICE – QUESTIONS & ANSWERS
