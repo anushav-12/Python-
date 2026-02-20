bbda15ed551278c8468506dfaf8b8f3deb21bd1e
Anusha V <116417089+anushav-12@users.noreply.github.com>
2026-02-20 22:49:27 +0530

    Variables

---
+++ 01_Variables.py
@@ -0,0 +1,106 @@
+# variables
+name = "Anusha"
+age = 23
+height = 5.2
+is_working = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_working))
+
+year_of_birth = 2026 - age
+print("Year of Birth:", year_of_birth)
+
+
+# Variables Practice Questions
+
+# 1️⃣ Create a variable for your country and print its type
+
+
+# 2️⃣ Create a variable for your dream company name and print it
+
+
+# 3️⃣ Create a variable for your graduation year and print its type
+
+
+# 4️⃣ Create a variable for your current CGPA and print its type
+
+
+# 5️⃣ Create two number variables and print their multiplication
+
+
+# 6️⃣ Create two variables:
+# one integer and one string
+# print both types
+
+
+# 7️⃣ Create a variable with your name in uppercase
+# print it in lowercase
+
+
+# 8️⃣ Create a variable score = 90
+# increase it by 10 using reassignment
+# print updated score
+
+
+# 9️⃣ Create a variable num = "100"
+# print num * 2
+
+
+# 🔟 Create two variables:
+# a = 15
+# b = 4
+# print:
+# a + b
+# a - b
+# a * b
+# a / b
+
+
+# 1️⃣1️⃣ Swap two variables:
+# x = 5
+# y = 10
+# After swapping print both
+
+
+# 1️⃣2️⃣ Create three variables in one line with values 1, 2, 3
+# print them
+
+
+# 1️⃣3️⃣ Assign same value 50 to three variables in one line
+# print them
+
+
+# 1️⃣4️⃣ Try creating a variable:
+# 1name = "Test"
+# What happens?
+
+
+# 1️⃣5️⃣ Try creating:
+# my-name = "Anusha"
+# What happens?
+
+
+# 1️⃣6️⃣ Create a variable using only underscore (_) as name
+# assign a value and print it
+
+
+# 1️⃣7️⃣ Create a variable:
+# x = 10
+# Reassign it to "Python"
+# print its type
+
+
+# 1️⃣8️⃣ Create two variables with same name in different lines
+# Assign different values
+# Print the variable
+
+
+# 1️⃣9️⃣ Fix this:
+# print("Year of Birth:", year_of_birth)
+
+
+# 2️⃣0️⃣ Create a variable birth_year
+# Calculate your age using current year
+# Print the result
